cb2da6e562419082f4cbb5be8443f004cfcebf7b
Philip Lemaster <philiplemaster@yahoo.com>
2020-04-29 19:45:18 -0400

    server.js connected to html

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const path = require('path');
 
-const mine = require('./scripts/mine');
-const fish = require('./scripts/fish');
+const mine = require('./public/scripts/mine');
+const fish = require('./public/scripts/fish');
 
 const app = express();
 const PORT = process.env.PORT || '8080';
 
 app.use(express.urlencoded({ extended: true}));
-
-app.get("/", function(req, res) {
-    res.status(200).send('BOTJS: Runescape Bot for Farming Lvls');
-});
+app.use(express.static('public'));
 
 app.listen(PORT, () => {
     console.log(`Listening on http:/localhost:${PORT}`);
 })
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.get('/mining', function(req, res) {
     mine.main();
